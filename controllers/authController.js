06498dd5c6787f4f1d05dd3486954d44ae7e81cf
MahmoudMuhammad <mahmoudmuhammadd0@gmail.com>
2024-05-05 22:46:21 +0200

    update forgot pass resetURL

--- controllers/authController.js
+++ controllers/authController.js
@@ -149,9 +149,15 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
+
+  /*
+    const resetURL = `${req.protocol}://${req.get(
     "host"
   )}/api/v1/users/resetPassword/${resetToken}`;
+  */
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/users/resetPassword?token=${resetToken}`;
   const message = `Forgot your password? Reset it by submitting a PATCH request with your new password and passwordConfirm to: ${resetURL}.\n\nIf you didn't forget your password, please ignore this email. Thank you!`;
   try {
     await sendEmail({
